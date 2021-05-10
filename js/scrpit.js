1c802d397d8c9900320c82778a9665a201327b72
Francine Hollweg <hollwegfrancine@gmail.com>
2021-05-10 19:51:34 -0300

    projeto painel completo.

---
+++ js/scrpit.js
@@ -0,0 +1,20 @@
+//a funçao ready serve para executar a função depois que a pagina ja estiver sido carregada.
+// ativando e desativando o menu oculto. do menu_painel
+
+$(document).ready(function(){
+
+	$(".setando").click(function(){
+
+	$(".menu").toggle()
+
+	})
+})
+
+$(document).ready(function(){
+
+	$(".botao").click(function(){
+
+	$(".sub_menu").toggle()
+
+	})
+})
